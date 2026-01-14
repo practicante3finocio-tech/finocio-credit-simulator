ba913fa447b97415fe9331ae3d040060184ccca2
Jose <practicante3finocio@gmail.com>
2026-01-14 12:23:19 +0100

    feature: added TicketPrinter58mm.java

---
+++ src/main/java/com/finocio/practicas/credit/simulator/infrastructure/TicketPrinter58mm.java
@@ -0,0 +1,11 @@
+package com.finocio.practicas.credit.simulator.infrastructure;
+
+
+public class TicketPrinter58mm extends AbstractTicketPrinter {
+
+    @Override
+    protected int getWidth() {
+        return 36;
+    }
+}
+
